a19b69e67889dea3753ef128476dacbc662d05ca
p3nal <abdullah.og@protonmail.com>
2023-12-17 17:12:20 +0100

    config changes

--- configuration.nix
+++ configuration.nix
@@ -13,6 +13,7 @@
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.configurationLimit = 10;
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
