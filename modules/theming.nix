267a5f56c97a22a94616c71e13a96218213185e0
p3nal <103528596+p3nal@users.noreply.github.com>
2023-12-21 22:43:23 +0100

    a whole bunch of stuff

--- modules/theming.nix
+++ modules/theming.nix
@@ -13,12 +13,8 @@
   config,
   ...
 }: let
-  gruvboxplus = import ./gruvbox-plus.nix {inherit pkgs;};
+  extraCss = "${builtins.readFile ../config/gtk/gtk.css}";
 in {
-  home.file = {
-    ".config/gtk-3.0/gtk.css".source = ../config/gtk-3.0/gtk.css;
-  };
-
   gtk = {
     enable = true;
     theme = {
@@ -27,23 +23,29 @@ in {
     };
     cursorTheme.name = "Bibata-Modern-Classic";
     iconTheme = {
-      package = gruvboxplus;
-      name = "GruvboxPlus";
+      package = pkgs.gruvbox-dark-icons-gtk;
+      name = "Gruvbox-Dark";
     };
     font = {
       name = "JetBrainsMono Bold";
       size = 10;
     };
-    gtk3.extraConfig = {
-      Settings = ''
-        gtk-application-prefer-dark-theme=1
-      '';
+    gtk3 = {
+      extraCss = extraCss;
+      extraConfig = {
+        Settings = ''
+          gtk-application-prefer-dark-theme=1
+        '';
+      };
     };
 
-    gtk4.extraConfig = {
-      Settings = ''
-        gtk-application-prefer-dark-theme=1
-      '';
+    gtk4 = {
+      extraCss = extraCss;
+      extraConfig = {
+        Settings = ''
+          gtk-application-prefer-dark-theme=1
+        '';
+      };
     };
     gtk2.configLocation = "${config.xdg.configHome}/gtk-2.0/gtkrc";
   };
