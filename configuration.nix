ef31c939f3af19efaba1632cae9d1f5954d6cda0
p3nal <103528596+p3nal@users.noreply.github.com>
2023-12-24 20:39:15 +0100

    welp, turns secret management under nixos is alot more messed up than i thought

--- configuration.nix
+++ configuration.nix
@@ -1,6 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 { config
 , lib
 , pkgs
@@ -22,9 +19,6 @@
   nixpkgs.config.allowUnfree = true;
 
   networking.hostName = "nixos"; # Define your hostname.
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
   networking.wireless.iwd.enable = true;
   networking.firewall.enable = true;
   systemd.network.enable = true;
@@ -39,7 +33,6 @@
   ];
   services.resolved = {
     enable = true;
-    # domains = [ "~." ];
     llmnr = "false";
     extraConfig = ''
       DNSOverTLS=yes
@@ -125,8 +118,6 @@
   #   useXkbConfig = true; # use xkb.options in tty.
   # };
 
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
   programs = {
     light.enable = true;
@@ -173,10 +164,6 @@
     pulse.enable = true;
     jack.enable = true;
   };
-  # hardware.pulseaudio.enable = true;
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.penal = {
@@ -184,7 +171,6 @@
     initialPassword = "password";
     extraGroups = [ "wheel" "video" "input" "docker" "libvirtd" ]; # Enable ‘sudo’ for the user.
     packages = with pkgs; [
-      # rofi-wayland
       dunst
       libnotify
       firefox
@@ -223,23 +209,6 @@
     ];
   };
 
-  # qt = {
-  #   enable = true;
-  #   style = "adwaita-dark";
-  # };
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
 
   # Open ports in the firewall.
   networking.firewall.allowedTCPPorts = [
@@ -255,14 +224,9 @@
   # sops-nix
   sops.defaultSopsFile = ./secrets/secrets.yaml;
   sops.defaultSopsFormat = "yaml";
-  sops.age.sshKeyPaths = [ "/home/penal/.ssh/id_ed25519" ];
-  # sops.age.keyFile = "/home/penal/.config/sops/age/keys.txt";
-  sops.secrets.example_key = {};
+  # sops.age.sshKeyPaths = [ "/home/penal/.ssh/id_ed25519" ];
+  sops.age.keyFile = "/home/penal/.config/sops/age/keys.txt";
 
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
 
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
