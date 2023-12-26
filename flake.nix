1982d33df315219ef226e848a4721350dfb3463b
p3nal <103528596+p3nal@users.noreply.github.com>
2023-12-27 00:48:32 +0100

    changed hm release

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
